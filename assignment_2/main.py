85b4e421d2ad57b381f2a35583b316c89a0fc0ab
jardw <jard.wesdorp@hotmail.com>
2023-10-24 14:14:50 +0200

    Able to print the radius around circle

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from matplotlib import pyplot as plt
 
@@ -8,26 +9,11 @@ from simulator import simulator
 
 def main():
     print("Starting main function")
-    
 
     # Create nodes here
-    nodes = []
-    nodes.append(Host(0, 4,5, 6, Aloha()))
-    nodes.append(Host(1, 3, 2, 18, Aloha()))
-    nodes.append(Host(2, 5, 2, 7, Aloha()))
-    nodes.append(Host(3, 1, 2, 13, Aloha()))
-    nodes.append(Host(4, 5, 4, 12, Aloha()))
-    nodes.append(Host(5, 1, 2, 3, Aloha()))
-    nodes.append(Host(6, 2, 3, 9, Aloha()))
-    nodes.append(Host(7, 4, 6, 4, Aloha()))
+    nodes = configure_nodes(4, ranges=[10, 10], max_radius=20)
 
-    plt.figure()
-    for node in nodes:
-        plt.plot(node.get_positionx(), node.get_positiony(), 'bo')
-
-    plt.show()
-
-    # Simulator is started here with a large timeout 
+    # Simulator is started here with a large timeout
     sim = simulator(nodes, 100000)
 
     started_calc = time.time()
@@ -36,7 +22,56 @@ def main():
 
     print(f"Calculation time {format(ended_calc - started_calc, '.4f')}")
 
-    sim.print_results() 
+    sim.print_results()
+    plot_points(nodes)
+
+
+def configure_nodes(number_of_nodes: int, ranges: [int, int], max_radius: int):
+    nodes = []
+    random.seed(10)
+
+    for id in range(number_of_nodes):
+        x = random.randint(0, ranges[0])
+        y = random.randint(0, ranges[1])
+
+        radius = random.randint(1, max_radius)
+
+        nodes.append(Host(id, x, y, radius, Aloha()))
+
+    return nodes
+
+def plot_points(nodes):
+    points = []
+    radius_all = []
+    ids = []
+    plt.figure()
+    for node in nodes:
+        x = node.get_positionx()
+        y = node.get_positiony()
+        radius = node.get_reach()
+        points.append((x, y))
+        radius_all.append(radius)
+        ids.append(node.mac)
+
+    def plot_circles_around_points(points, radius_all, ids):
+        for (x, y), radius, id in zip(points, radius_all, ids):
+            circle = plt.Circle((x, y), radius, color='blue', fill=False)
+            plt.gca().add_patch(circle)
+            plt.text(x, y - 0.2 * radius, id, ha='center', va='center')
+
+    x, y = zip(*points)
+    plt.scatter(x, y, color='red', label='Points')
+
+    plot_circles_around_points(points, radius_all, ids)
+
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Points with Circles')
+    plt.legend()
+    plt.axis('equal')
+    plt.grid(True)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
